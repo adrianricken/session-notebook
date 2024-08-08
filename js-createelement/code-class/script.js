fdda5a95cdeea5a0810ccf449c5239ebfafea269
Adrian Ricken <adrianricken@gmail.com>
2024-08-08 10:02:28 +0200

    code along class

--- js-createelement/code-class/script.js
+++ js-createelement/code-class/script.js
@@ -1,1 +1,25 @@
 console.clear();
+
+const form = document.querySelector('[data-js="card-form"]');
+
+// Verify I am "grabbing" the form correctly
+// console.log('form: ', form);
+
+element.addEventListener('eventName', function)
+
+1. Arrow function
+form.addEventListener('submit', (event) {
+    event.preventDefault();
+    console.log('I am in the event in the arrow function');
+});
+
+2. creating a function outside and pass it 
+function handleSubmit(event){
+    event.preventDefault();
+    console.log("i am in the event, calling an other function");
+}
+
+3. form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log('i am in the event function');
+});
